ce841bc7f4dc5899157bb75a8e38c82fb5220c46
rhekdms <130122322+rhekdms@users.noreply.github.com>
2023-05-29 18:11:15 +0900

    first commoi

---
+++ nightday.js
@@ -0,0 +1,34 @@
+
+var Link = {
+    setColor:function(color){
+        //var alist = document.querySelectorAll('a');
+        //var i = 0;
+        //while (i < alist.length) {
+        //    alist[i].style.color = color;
+        //    i = i + 1;
+        //}
+        $('a').css('color', color);
+    }
+}
+var Body = {
+    setColor:function(color){
+        document.querySelector('body').style.color = color;
+    },
+    setBackgroundColor: function(color){
+        document.querySelector('body').style.backgroundColor = color;
+    }
+}
+function nightdayhandler(self){
+    var target = document.querySelector('body')
+    if(self.value==='night'){
+        Body.setBackgroundColor('black');
+        Body.setColor('white');
+        self.value='day';
+        Link.setcolor('powderblue')
+    } else {
+        Body.setBackgroundColor('white');
+        Body.setColor('black');
+        self.value='night';
+        Link.setcolor('blue')
+    }
+}

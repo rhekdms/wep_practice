d7e06d024490352008631c8020e987e6c31f78f5
rhekdms <rhekdms0507@gmail.com>
2023-06-23 22:18:35 +0900

    230623 9시간동안 했는데에에에ㅔ에에에ㅔ.......

--- 230624/230621res.js
+++ 230624/230621res.js
@@ -31,3 +31,64 @@ function REMEMBER(){
         document.querySelector('.Remember').style.display='block';
     }
 }
+function WRITE(){
+    document.querySelector('.Write').style.display='block';
+    document.querySelector('.OnlyPicture').style.display='none';
+    document.querySelector('.OnlyWriting').style.display='none';
+    document.querySelector('.To_Do').style.display='none';
+    document.querySelector('.Memo').style.display='none';
+    document.querySelector('.Calender').style.display='none';
+}
+
+function ONLY_PICTURE(){
+    document.querySelector('.Write').style.display='none';
+    document.querySelector('.OnlyPicture').style.display='block';
+    document.querySelector('.OnlyWriting').style.display='none';
+    document.querySelector('.To_Do').style.display='none';
+    document.querySelector('.Memo').style.display='none';
+    document.querySelector('.Calender').style.display='none';
+}
+
+function ONLY_WRITING(){
+    document.querySelector('.Write').style.display='none';
+    document.querySelector('.OnlyPicture').style.display='none';
+    document.querySelector('.OnlyWriting').style.display='block';
+    document.querySelector('.To_Do').style.display='none';
+    document.querySelector('.Memo').style.display='none';
+    document.querySelector('.Calender').style.display='none';
+}
+
+function PICTURE_N_WRITING(){
+    document.querySelector('.Write').style.display='none';
+    document.querySelector('.OnlyPicture').style.display='block';
+    document.querySelector('.OnlyWriting').style.display='block';
+    document.querySelector('.To_Do').style.display='none';
+    document.querySelector('.Memo').style.display='none';
+    document.querySelector('.Calender').style.display='none';
+}
+
+function MEMO(){
+    document.querySelector('.Write').style.display='none';
+    document.querySelector('.OnlyPicture').style.display='none';
+    document.querySelector('.OnlyWriting').style.display='none';
+    document.querySelector('.To_Do').style.display='none';
+    document.querySelector('.Memo').style.display='block';
+    document.querySelector('.Calender').style.display='none';
+}
+
+function TO_DO(){
+    document.querySelector('.Write').style.display='none';
+    document.querySelector('.OnlyPicture').style.display='none';
+    document.querySelector('.OnlyWriting').style.display='none';
+    document.querySelector('.To_Do').style.display='block';
+    document.querySelector('.Memo').style.display='none';
+    document.querySelector('.Calender').style.display='none';
+}
+function PHOTO_DIARY(){
+    document.querySelector('.Write').style.display='none';
+    document.querySelector('.OnlyPicture').style.display='none';
+    document.querySelector('.OnlyWriting').style.display='none';
+    document.querySelector('.To_Do').style.display='block';
+    document.querySelector('.Memo').style.display='block';
+    document.querySelector('.Calender').style.display='block';
+}

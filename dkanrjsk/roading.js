9748e7bedbfb203114ef2e7ebdf805e75907ff1d
rhekdms <rhekdms0507@gmail.com>
2023-08-16 18:04:43 +0900

    230816

--- dkanrjsk/roading.js
+++ dkanrjsk/roading.js
@@ -1,11 +1,4 @@
 function f(n){
     $(function(){
         $('#screen_change').load(n);
-        document.querySelector('#back').style.display='block';
-    });}
-
-function BACK(){
-    $(function(){
-        $('#screen_change').load('start.html');
-        document.querySelector('#back').style.display='none';
     });}
